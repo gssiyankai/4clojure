8420af2d2d59bf4f4a86c00bc637603bd7f38ac7
gssiyankai <syk.greg@gmail.com>
2013-11-08 23:08:34 +0100

    tic tac toe

--- src/tic_tac_toe.clj
+++ src/tic_tac_toe.clj
@@ -2,10 +2,10 @@
 
 (defn winner [[x y z :as grid-by-lines]]
   (let [column (fn [f] (map f grid-by-lines))
-        grid-by-columns (map column (list first second last))
-        diagonal (fn [grid] (map #(%1 %2) (list first second last) grid))
-        diagonals (map diagonal (list grid-by-lines (reverse grid-by-lines)))
-        is-every (fn [symbol a] (every? #(= symbol %) a))
+        grid-by-columns (map column [first second last])
+        diagonal (fn [grid] (map #(%1 %2) [first second last] grid))
+        diagonals (map diagonal [grid-by-lines (reverse grid-by-lines)])
+        is-every (fn [symbol coll] (every? #(= symbol %) coll))
         is-winner (fn [user] (some #(is-every user %) (concat grid-by-lines grid-by-columns diagonals)))]
     (cond
       (is-winner :x) :x
