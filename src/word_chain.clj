5514e15a48fc73bab4dd09f878507ac27c7c8d25
gssiyankai <syk.greg@gmail.com>
2013-11-15 01:01:16 +0100

    word chain

--- src/word_chain.clj
+++ src/word_chain.clj
@@ -1,18 +1,56 @@
 (ns word_chain)
 
-(defn can-chain [words]
-  (let [remove-common-leading-chars (fn remove [word1 word2] (if (or (empty? word1) (empty? word2) (not= (first word1) (first word2)))
-                                                        [word1 word2]
-                                                        (remove (rest word1) (rest word2))))
-        remove-common-trailing-chars (fn [[word1 word2]] (map #(apply str (reverse %)) (remove-common-leading-chars (reverse word1) (reverse word2))))
-        remove-common-leading-and-trailing-chars (fn [word1 word2] (remove-common-trailing-chars (remove-common-leading-chars word1 word2)))
-        can-chain-with-modification (fn [counts word1 word2] (= counts (map #([(count (first %)) (count (second %))]) (remove-common-leading-and-trailing-chars word1 word2))))
-        can-chain-with-substitution (fn [word1 word2] (can-chain-with-modification [1 1] word1 word2))
-        can-chain-with-insertion (fn [word1 word2] (can-chain-with-modification [0 1] word1 word2))
-        can-chain-with-deletion (fn [word1 word2] (can-chain-with-insertion word2 word1))]
-    (or (can-chain-with-substitution (first words) (second words))
-        (can-chain-with-insertion (first words) (second words))
-        (can-chain-with-deletion (first words) (second words))
+(defn remove-common-leading-chars [[word1 word2]]
+  (if (or (empty? word1)
+          (empty? word2)
+          (not= (first word1) (first word2)))
+              [(apply str word1) (apply str word2)]
+              (remove-common-leading-chars [(rest word1) (rest word2)])
+  )
+)
+
+(defn remove-common-trailing-chars [[word1 word2]]
+  (map
+    #(apply str (reverse %))
+    (remove-common-leading-chars [(reverse word1) (reverse word2)])
+  )
+)
+
+(defn strip [[word1 word2]]
+  (remove-common-trailing-chars
+    (remove-common-leading-chars [word1 word2]))
+)
+
+(defn can-chain-with-modification [size word1 word2]
+  (= size (map count (strip [word1 word2])))
+)
+
+(defn can-chain-with-substitution [word1 word2]
+  (can-chain-with-modification [1 1] word1 word2)
+)
+
+(defn can-chain-with-insertion [word1 word2]
+  (can-chain-with-modification [0 1] word1 word2)
+)
+
+(defn can-chain-with-deletion [word1 word2]
+  (can-chain-with-insertion word2 word1)
+)
+
+(defn can-chain
+  ([words] (can-chain (set (rest words)) (first words) nil))
+  ([words first_chain_word last_chain_word]
+    (let [chain-words (fn [word] (filter #(or (can-chain-with-substitution % word)
+                                              (can-chain-with-insertion % word)
+                                              (can-chain-with-deletion % word))
+                                         words))
+          head-chain-words (chain-words first_chain_word)
+          tail-chain-words (chain-words last_chain_word)]
+      (or (empty? words)
+          (if (nil? last_chain_word) (some #(can-chain (disj words %) first_chain_word %) head-chain-words)
+                                     (some #(can-chain (disj words %) % last_chain_word) head-chain-words))
+          (and (not (nil? last_chain_word)) (some #(can-chain (disj words %) first_chain_word %) tail-chain-words))
+      )
     )
   )
 )
