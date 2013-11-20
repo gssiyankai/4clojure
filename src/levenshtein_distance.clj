10c0e9bb0fa82caa5917d90dc0031722422e3002
gssiyankai <syk.greg@gmail.com>
2013-11-20 23:16:57 +0100

    levenshtein distance

--- src/levenshtein_distance.clj
+++ src/levenshtein_distance.clj
@@ -1,12 +1,15 @@
 (ns levenshtein_distance)
 
 (defn distance [str1 str2]
-  (let [len1 (count str1)
-        len2 (count str2)
-        match (= (first str1) (first str2))]
-    (cond (zero? len1) len2
-          (zero? len2) len1
-          (true? match) (distance (rest str1) (rest str2))
-          :else (min (inc (distance (rest str1) str2))
-                     (inc (distance str1 (rest str2)))
-                     (inc (distance (rest str1) (rest str2)))))))
+  (let [f (fn [lev str1 str2]
+            (let [len1 (count str1)
+                  len2 (count str2)
+                  match (= (first str1) (first str2))]
+              (cond (zero? len1) len2
+                (zero? len2) len1
+                (true? match) (lev lev (rest str1) (rest str2))
+                :else (min (inc (lev lev (rest str1) str2))
+                        (inc (lev lev str1 (rest str2)))
+                        (inc (lev lev (rest str1) (rest str2)))))))]
+    (f (memoize f) (vec str1) (vec str2)))
+)
