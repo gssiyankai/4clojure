a2f0fd98c3c275eef94ddb72612c5b62f2adebb4
gssiyankai <syk.greg@gmail.com>
2013-11-21 12:11:01 +0100

    crossword puzzle

--- src/crossword_puzzle.clj
+++ src/crossword_puzzle.clj
@@ -1,6 +1,17 @@
 (ns crossword_puzzle)
 
-
-(defn can-place [& arg]
-  nil
+(defn can-place [word board]
+  (let [partial-line-words (fn [line] (->> (filter #(not= % \space) line)
+                                           (partition-by #(= % \#))
+                                           (filter #(not= % '(\#)))
+                                           (map #(apply str %))))
+        partial-words (flatten (vector (map partial-line-words board)
+                                       (map vector (flatten board))))
+        candidate (fn [partial-word] (and (= (count word) (count partial-word))
+                                          (->> (map vector word partial-word)
+                                               (drop-while #(or (= (second %) \_) (apply = %)))
+                                               (empty?))))
+        candidates (filter #(candidate %) partial-words)]
+      (not (empty? candidates))
+    )
 )
