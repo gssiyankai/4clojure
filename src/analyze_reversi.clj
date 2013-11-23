8b788278bdc05400f8bd35c59be11a57df792cfa
gssiyankai <syk.greg@gmail.com>
2013-11-23 10:35:37 +0100

    analyze reversi

--- src/analyze_reversi.clj
+++ src/analyze_reversi.clj
@@ -1,6 +1,45 @@
 (ns analyze_reversi)
 
+(defn coordinates [board player]
+  (for [x (range (count board))
+        y (range (count (first board)))
+        :when (= player ((board x) y))]
+    [x y]
+  )
+)
 
-(defn moves [board color]
-  nil
+(defn opponent [player]
+  (cond (= player 'w) 'b
+        (= player 'b) 'w)
+)
+
+(defn moves
+  ([board player]
+    (let [empty-coordinates (coordinates board 'e)
+          opponent (opponent player)
+          opponent-coordinates (set (coordinates board opponent))
+          player-coordinates (set (coordinates board player))]
+      (->>
+            (apply merge (map #(hash-map % (for [f-x [inc identity dec]
+                                                f-y [inc identity dec]]
+                                            (moves player-coordinates opponent-coordinates % f-x f-y)))
+                             empty-coordinates))
+           (map (fn [entry] [(first entry) (filter #(> (count %) 1) (second entry))]))
+           (map (fn [entry] [(first entry) (filter #(every? (fn [arg] (not (nil? arg))) %) (second entry))]))
+           (map (fn [entry] [(first entry) (filter #(contains? player-coordinates (last %)) (second entry))]))
+           (map (fn [entry] [(first entry) (filter #(vector? (first %)) (second entry))]))
+           (map (fn [entry] [(first entry) (map #(drop-last %) (second entry))]))
+           (filter (fn [entry] (not (empty? (second entry)))))
+           (map (fn [entry] [(first entry) (apply set (second entry))]))
+           (into {})
+      )
+    )
+  )
+  ([player-coordinates opponent-coordinates current-coordinates f-x f-y]
+    (let [next-coordinates [(f-x (first current-coordinates))(f-y (second current-coordinates))]]
+      (cond (contains? opponent-coordinates next-coordinates) (cons next-coordinates (moves player-coordinates opponent-coordinates next-coordinates f-x f-y))
+            (contains? player-coordinates next-coordinates) [next-coordinates]
+      )
+    )
+  )
 )
--- test/analyze_reversi_test.clj
+++ test/analyze_reversi_test.clj
@@ -2,6 +2,25 @@
   (:use clojure.test)
   (:use analyze_reversi))
 
+(deftest test-0
+  (is (= [[1 1] [2 2 ]]
+        (coordinates '[[e e e e]
+                       [e w b e]
+                       [e b w e]
+                       [e e e e]] 'w)))
+  (is (= [[1 2] [2 1]]
+        (coordinates '[[e e e e]
+                       [e w b e]
+                       [e b w e]
+                       [e e e e]] 'b)))
+  (is (= 'b (opponent 'w)))
+  (is (= 'w (opponent 'b)))
+  (is (= { [1 3] #{[1 2]} }
+        (moves '[[e e e e]
+                 [e w b e]
+                 [e e e e]
+                 [e e e e]] 'w))))
+
 (deftest test-1
   (is (= {[1 3] #{[1 2]}, [0 2] #{[1 2]}, [3 1] #{[2 1]}, [2 0] #{[2 1]}}
         (moves '[[e e e e]
