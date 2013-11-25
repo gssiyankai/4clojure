5bcf716560cfbbf3e6b5b2b89922d69d9e39ff32
gssiyankai <syk.greg@gmail.com>
2013-11-25 15:37:00 +0100

    read roman numerals

--- test/read_roman_numerals_test.clj
+++ test/read_roman_numerals_test.clj
@@ -11,19 +11,20 @@
   (is (= 500  (read-roman-numeral "D")))
   (is (= 1000 (read-roman-numeral "M")))
   (is (= 2    (read-roman-numeral "II")))
-  (is (= 4    (read-roman-numeral "IV"))))
+  (is (= 4    (read-roman-numeral "IV")))
+  (is (= 9    (read-roman-numeral "IX"))))
 
-;(deftest test-1
-;  (is (= 14 (read-roman-numeral "XIV"))))
-;
-;(deftest test-2
-;  (is (= 827 (read-roman-numeral "DCCCXXVII"))))
-;
-;(deftest test-3
-;  (is (= 3999 (read-roman-numeral "MMMCMXCIX"))))
-;
-;(deftest test-4
-;  (is (= 48 (read-roman-numeral "XLVIII"))))
+(deftest test-1
+  (is (= 14 (read-roman-numeral "XIV"))))
+
+(deftest test-2
+  (is (= 827 (read-roman-numeral "DCCCXXVII"))))
+
+(deftest test-3
+  (is (= 3999 (read-roman-numeral "MMMCMXCIX"))))
+
+(deftest test-4
+  (is (= 48 (read-roman-numeral "XLVIII"))))
 
 
 (run-tests 'read_roman_numerals_test)
