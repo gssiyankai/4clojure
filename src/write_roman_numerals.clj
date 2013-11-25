d2610616e6c78df0ae0db75299a9d6cecac2f5c4
gssiyankai <syk.greg@gmail.com>
2013-11-25 15:52:10 +0100

    write roman numerals

---
+++ src/write_roman_numerals.clj
@@ -0,0 +1,5 @@
+(ns write_roman_numerals)
+
+(defn write-roman-numeral [number]
+  nil
+)
---
+++ test/write_roman_numerals_test.clj
@@ -0,0 +1,27 @@
+(ns write_roman_numerals_test
+  (:use clojure.test)
+  (:use write_roman_numerals))
+
+(deftest test-1
+  (is (= "I" (write-roman-numeral 1))))
+
+(deftest test-2
+  (is (= "XXX" (write-roman-numeral 30))))
+
+(deftest test-3
+  (is (= "IV" (write-roman-numeral 4))))
+
+(deftest test-4
+  (is (= "CXL" (write-roman-numeral 140))))
+
+(deftest test-5
+  (is (= "DCCCXXVII" (write-roman-numeral 827))))
+
+(deftest test-5
+  (is (= "MMMCMXCIX" (write-roman-numeral 3999))))
+
+(deftest test-5
+  (is (= "XLVIII" (write-roman-numeral 48))))
+
+
+(run-tests 'write_roman_numerals_test)
