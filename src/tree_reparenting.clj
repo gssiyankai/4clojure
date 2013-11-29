b6adbe5c595528bb4dae7facd5730eeb2fd109e9
gssiyankai <syk.greg@gmail.com>
2013-11-29 01:29:39 +0100

    tree reparenting

--- src/tree_reparenting.clj
+++ src/tree_reparenting.clj
@@ -1,22 +1,20 @@
 (ns tree_reparenting)
 
 
-(defn parse [cache node parent children]
-  (merge {node [parent (map first children)]}
-         (apply merge (map #(parse cache (first %) node (rest %)) children)))
-)
-
-(defn connect [root cache]
-  (let [children-and-parent (->> (cond (nil? (first (cache root))) (second (cache root))
-                                        :else (flatten [(second (cache root)) (first (cache root))]))
-                                 (filter #(contains? cache %)))
-        new-cache (dissoc cache root)]
-    (cons root (map #(connect % new-cache) children-and-parent))
-  )
-)
-
 (defn reconnect [node tree]
-  (let [cache (parse {} (first tree) nil (rest tree))
+  (let [parse (fn parse [node parent children]
+                (merge {node [parent (map first children)]}
+                  (apply merge (map #(parse (first %) node (rest %)) children)))
+              )
+        cache (parse (first tree) nil (rest tree))
+        connect (fn connect [root cache]
+                  (let [children-and-parent (->> (cond (nil? (first (cache root))) (second (cache root))
+                                                       :else (flatten [(second (cache root)) (first (cache root))]))
+                                                 (filter #(contains? cache %)))
+                        new-cache (dissoc cache root)]
+                    (cons root (map #(connect % new-cache) children-and-parent))
+                  )
+                )
         new-tree (connect node cache)]
     new-tree
   )
