d747e221df443239347eb91914e68fb40fcd734d
gssiyankai <syk.greg@gmail.com>
2013-12-11 00:33:40 +0100

    graph connectivity

--- src/graph_connectivity.clj
+++ src/graph_connectivity.clj
@@ -1,11 +1,11 @@
 (ns graph_connectivity)
 
-(defn connected [edges]
-  (let [vertices (set (apply concat edges))
+(defn connected [graph]
+  (let [vertices (set (apply concat graph))
         connected-vertices (fn connected-vertices
                              ([vertex] (connected-vertices vertex #{}))
                              ([vertex cache]
-                               (let [adjacent-vertices (->> (apply concat (filter #(contains? (set %) vertex) edges))
+                               (let [adjacent-vertices (->> (apply concat (filter #(contains? (set %) vertex) graph))
                                                             (filter #(not (contains? cache %)))
                                                        )]
                                  (if (empty? adjacent-vertices)
