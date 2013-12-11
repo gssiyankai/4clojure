e3dab6de98f00d0b8155fb6ffe4ec37a18784691
gssiyankai <syk.greg@gmail.com>
2013-12-11 22:56:16 +0100

    palindromic numbers

--- src/palindromic_numbers.clj
+++ src/palindromic_numbers.clj
@@ -1,5 +1,16 @@
 (ns palindromic_numbers)
 
-(defn num-seq [start]
-    nil
+(defn pal-num [n]
+  (let [s (str n)
+        length (count s)
+        half (subs s 0 (Math/ceil (/ length 2)))
+        next-half (str (inc (read-string half)))
+        next-pal (first
+                   (filter #(>= % n)
+                     (map #(read-string (str %
+                                            (subs (clojure.string/reverse %) (if (even? length) 0 1))))
+                          [half next-half])
+                     )
+                 )]
+    (cons next-pal (lazy-seq (pal-num (inc next-pal)))))
 )
--- test/palindromic_numbers_test.clj
+++ test/palindromic_numbers_test.clj
@@ -3,37 +3,37 @@
   (:use palindromic_numbers))
 
 (deftest test-1
-  (is (= (take 26 (num-seq 0))
+  (is (= (take 26 (pal-num 0))
           [0 1 2 3 4 5 6 7 8 9
            11 22 33 44 55 66 77 88 99
            101 111 121 131 141 151 161])))
 
 (deftest test-2
-  (is (= (take 16 (num-seq 162))
+  (is (= (take 16 (pal-num 162))
          [171 181 191 202
           212 222 232 242
           252 262 272 282
           292 303 313 323])))
 
 (deftest test-3
-  (is (= (take 6 (num-seq 1234550000))
+  (is (= (take 6 (pal-num 1234550000))
          [1234554321 1234664321 1234774321
           1234884321 1234994321 1235005321])))
 
 (deftest test-4
-  (is (= (first (num-seq (* 111111111 111111111)))
+  (is (= (first (pal-num (* 111111111 111111111)))
          (* 111111111 111111111))))
 
 (deftest test-5
-  (is (= (set (take 199 (num-seq 0)))
-        (set (map #(first (num-seq %)) (range 0 10000))))))
+  (is (= (set (take 199 (pal-num 0)))
+        (set (map #(first (pal-num %)) (range 0 10000))))))
 
 (deftest test-6
   (is (= true
-        (apply < (take 6666 (num-seq 9999999))))))
+        (apply < (take 6666 (pal-num 9999999))))))
 
 (deftest test-6
-  (is (= (nth (num-seq 0) 10101)
+  (is (= (nth (pal-num 0) 10101)
          9102019)))
 
 
