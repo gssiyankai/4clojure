dc224bd50c3cdb0f4214580d0ec0bfa0d1993867
gssiyankai <syk.greg@gmail.com>
2013-12-20 15:21:29 +0100

    dfa

--- src/dfa.clj
+++ src/dfa.clj
@@ -1,5 +1,20 @@
 (ns dfa)
 
-(defn language [definition]
-  nil
+(defn language
+  ([definition]
+    (let [start (definition :start)
+          transitions (definition :transitions)
+          accepts (definition :accepts)]
+      (language transitions start accepts "")
+    )
+  )
+  ([transitions start accepts word]
+    (let [acceptable-starts (transitions start)
+          acceptable-states (filter #(contains? accepts (val %)) acceptable-starts)
+          acceptable-words (map #(str word (key %)) acceptable-states)
+          next-words (map #(vector (str word (key %)) (val %)) acceptable-starts)
+          next-transitions (dissoc transitions start)]
+      (flatten (conj acceptable-words (lazy-seq (map #(language next-transitions (second %) accepts (first %)) next-words))))
+    )
+  )
 )
