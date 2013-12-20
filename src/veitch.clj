1df49155555ba76fd1efd5f7bafe671235b8d09f
gssiyankai <syk.greg@gmail.com>
2013-12-20 19:31:17 +0100

    veitch

--- src/veitch.clj
+++ src/veitch.clj
@@ -3,16 +3,21 @@
 (use 'clojure.set)
 
 (defn simplify [inputs]
-  (let [filter-candidates (fn [input candidates]
+  (let [functions {'A 'a, 'a 'A, 'B 'b, 'b 'B, 'C 'c, 'c 'C, 'D 'd, 'd 'D}
+        filter-candidates (fn [input candidates]
                             (->> (filter #(= (count input) (count %)) candidates)
-                                 (filter #(= (dec (count input)) (count (intersection input %))))))
-        filter-inputs (fn [input candidate] (intersection))
+                                 (filter #(= (dec (count input)) (count (intersection input %))))
+                                 (filter #(contains? % (functions (first (difference input %)))))))
         simplified-inputs (->> (map #(vector % (disj inputs %)) inputs)
                                (map #(vector (first %) (first (filter-candidates (first %) (second %)))))
                                (map #(if (empty? (second %))
                                        (first %)
                                        (apply intersection %)))
-                               (into #{}))]
+                               (sort #(compare (count %1) (count %2)))
+                               (reduce (fn [acc i] (if (some #(subset? % i) acc)
+                                                     acc
+                                                     (conj acc i)))
+                                       #{}))]
     (if (= inputs simplified-inputs)
       inputs
       (simplify simplified-inputs))
