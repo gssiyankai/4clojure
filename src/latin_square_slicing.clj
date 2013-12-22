58a9d11378bd4c5f129b90116e4f3ff8c25f4a0f
gssiyankai <syk.greg@gmail.com>
2013-12-22 12:43:31 +0100

    latin square slicing

--- src/latin_square_slicing.clj
+++ src/latin_square_slicing.clj
@@ -1,5 +1,20 @@
 (ns latin_square_slicing)
 
+(defn latin-square? [square]
+  (let [rows (identity square)
+        columns (reduce #(map flatten (map vector %1 %2)) rows)
+        distinct-elements (map #(distinct %) (concat rows columns))
+        distinct-square-elements (reduce #(apply conj %1 %2) #{} distinct-elements)]
+    (apply = (conj (map #(count %) distinct-elements)
+                   (count square)
+                   (count (first square))
+                   (count distinct-square-elements)))
+  )
+)
+
 (defn process [vectors]
-  {}
+  (let [rows (identity vectors)
+        columns (reduce #(map flatten (map vector %1 %2)) rows)]
+    {}
+  )
 )
