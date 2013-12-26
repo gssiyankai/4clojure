958276d957bbe70b9b389ff04972788f4d2000f3
gssiyankai <syk.greg@gmail.com>
2013-12-26 12:32:40 +0100

    squares squared

--- src/squares_squared.clj
+++ src/squares_squared.clj
@@ -1,8 +1,20 @@
 (ns squares_squared)
 
+(defn square [x]
+  (* x x)
+)
+
 (defn squares [start end]
   (when (<= start end)
-    (cons start (lazy-seq (squares (* start start) end)))
+    (cons start (lazy-seq (squares (square start) end)))
+  )
+)
+
+(defn tokenize [squares]
+  (let [digits (mapcat str squares)
+        size (count digits)
+        dim (int (Math/ceil (Math/sqrt size)))]
+    (concat digits (repeat (- (square dim) size) \*))
   )
 )
 
