fab5a7cdf112316289130fab36e3aa8d955259b0
gssiyankai <syk.greg@gmail.com>
2013-12-26 04:55:22 +0100

    love triangle

--- src/love_triangle.clj
+++ src/love_triangle.clj
@@ -8,10 +8,35 @@
         max-bitmap-size (reduce #(max %1 (count %2)) 0 bs)
         pad (fn [bitmap p]
               (concat (repeat (- p (count bitmap)) 0) bitmap))]
-    (map #(pad % max-bitmap-size) bs)
+    (vec (map #(vec (pad % max-bitmap-size)) bs))
   )
 )
 
-(defn area [mine]
-  nil
+(defn triangle [bitmap height width surface x y dx dy]
+  (let [x2 (+ x dx)
+        y2 (+ y dy)]
+    (if (and (< x2 width)
+             (< y2 height)
+             (every? #(= 1 %)
+               (for [delta (range dx)]
+                 (get-in bitmap [(- y2 delta) (+ x delta)])
+               )
+             ))
+      (triangle bitmap height width (+ surface (inc dx)) x y (inc dx) (inc dy))
+      surface
+    )
+  )
+)
+
+(defn area [[& mine]]
+  (let [bitmap (apply to-binary mine)
+        height (count bitmap)
+        width (count (first bitmap))]
+    (apply max
+      (for [x (range height)
+            y (range width)]
+        (triangle bitmap height width 1 x y 1 -1)
+      )
+    )
+  )
 )
